f5095f4c9ad95631517e081375f46c16944525dc
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-09 00:57:17 +0300

    movie

--- app.js
+++ app.js
@@ -1,37 +1,109 @@
-const express=require ("express");
+const express = require ('express');
 const app=express ()
 const port=3000 ;
+
+
+
+
+
 app.use(express.json()) ;
 
+const fs = require("fs");
 
-const movies=[{id:1, name:"walking",isfav:false, isdelete:true },
-{id:2, name:"walking",isfav:false, isdelete:true },
-{id:3, name:"walking",isfav:false, isdelete:true }];
+
+let movies = [];
+
 
 //to show all items arry
-app.get("/movies",(req,res)=>{
+app.get("/movies",(req,res)=> {
     res.status(200);
     res.json(movies) ;
     }) ;
 
-    app.get("/movie_id",(req,res)=>  {
+    
+    app.get("/movie/:id",(req,res)=> {
+        const found = movies.find((found) => found.id === parseInt(req.params.id));
+      
 
-   const id = req.query.id;
-   const movie = movies.find((movie)=>
+        if(found)
+        {
 
-   movie.id===Number(id));
-   res.status(200);
-    res.json(movie) ;
-    }) ;
+            res.json(movies.filter(movie=>movie.id===parseInt(req.params.id)));
+        }
+        else
+        {
 
-   
-   
-   
+            res.status(400).json({msg:`movie not found`});
+        
+        
+        }
+      });
+
+            
+
+      
+app.post("/movies", (req, res) => {
+    const {id,name,isfav}=req.body;
+
+movies.push({id:movies.length+1+"",
+name:name,
+isfav:isfav,
+isdelete:false
+})
+
+    fs.readFile("./read.json", (err, data) => {
+        let movies = [];
+          movies = JSON.parse(data.toString());
+        });
+  ​
+
+  app.put("/movies/:id", (req, res) => {
+    const updated = movies.map((ele) => {
+      if (ele.id == req.params.id) {
+        return {
+          id: ele.id,
+          name: req.body.name,
+          isFav: req.body.isFav,
+          isdelete: req.body.isDeleted,
+        };
+      } else return ele;
+    });
+    fs.writeFile("./movies.json", JSON.stringify(updated), (err, data) => {});
+    let filter = movies.filter((ele) => ele.isdelete === "false");
+    res.json(filter);
+  });
   
+  app.delete("/delete/:id", (req, res) => {
+    let delet = movies.map((ele) => {
+      if (ele.id == req.params.id) {
+         
+        return {
+          id: ele.id,
+          name: ele.name,
+          isfav: ele.isFav,
+          isdelete: "true"
+        };
+      } else return ele;
+    });
+    let filter = delet.filter((ele) => ele.isdelete === "false");
+    fs.writeFile("./movies.json", JSON.stringify(delet),(err,data)=>{});
+    res.json(filter);
+  });
+  //app.put('/movie/:id' ,(req ,res)=>
+  //{
+  //  const {id,name,isfav,isdelete}=req.body;
+  //  movies.forEach(movie=>{
 
-
-
+       // if(movie.id===parseInt(req.params.id))
+       // return {
+         //   id: element.id,
+          //  name: req.body.name,
+           // isFav: req.body.isFav,
+          ///  isDeleted: req.body.isDeleted,
+        //  };
+       // } else return element;
+     // });
 
             app.listen (port,() => {
-                console.log(`server is running ${port}`);
-                });
+                console.log(`server is running ${port}`)
+            });
